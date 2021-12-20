d1ee18acb58d56aa9d221a613e764235825008f1
rokeyaetiphero <roserapunzel96@gmail.com>
2021-12-20 09:03:09 +0600

    my-commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+
+const Shop = () => {
+    console.log(fakeData);
+    return (
+        <div>
+            <h1>This is Shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
